c333de1138fd2e7c3442a977a6841f6c057477e5
dxwc <dxwc@users.noreply.github.com>
2019-01-17 12:48:59 -0500

    markdown details and cleanup

--- load_to_db/tools/generate_page_dir.js
+++ load_to_db/tools/generate_page_dir.js
@@ -6,7 +6,13 @@ Promise.resolve()
 .then(() =>
 {
     return db
-    .many(`SELECT *, to_char(receipt, 'mm/dd/yyyy') AS receipt FROM candidate`);
+    .many
+    (
+        `SELECT
+            *,
+            to_char(receipt, 'mm/dd/yyyy') AS receipt
+        FROM candidate
+        ORDER BY candidate.receipt DESC`);
 })
 .then((data) =>
 {
@@ -15,26 +21,52 @@ Promise.resolve()
 `---
 title: All ${data.length} Candidates Who Filed For 2020 Presidency
 ---
+
+
+<style>
+table
+{
+    display: table;
+    margin: 0 auto;
+}
+
+tr:nth-child(odd)
+{
+    background-color: #efefef;
+}
+
+h1, h2
+{
+    text-align: center;
+}
+</style>
 `
     );
 
     let parties = { };
 
+    console.log('<table>');
     data.forEach((can) =>
     {
         if(!parties[can.party]) parties[can.party] = 1;
         else                    parties[can.party]++;
         console.log
         (
-`
-<span class='candidate'>
-    <h3 class='name' title='${can.id}'>
-        <a id='${can.id}' href='/candidate/${can.id.trim()}.html'>${can.full_name}</a>
-    </h3>
-    Party: <b class='party'>${can.party}</b><br>
-</span>`
+`<tr>
+    <td>
+        <a
+            id='${can.id}'
+            href='/candidate/${can.id.trim()}.html'>${can.full_name}
+        </a>
+    </td>
+    <td>
+        ${can.party}
+    </td>
+</tr>`
         );
     });
+    console.log('</table>');
+
     console.log(`<h2>Number of Parties and Candidates in each party</h2>`)
     console.log(`<table>`);
     parties = Object.entries(parties).sort((a, b) => b[1] - a[1]);
@@ -43,20 +75,6 @@ title: All ${data.length} Candidates Who Filed For 2020 Presidency
         console.log(`<tr><td>${p[0]}</td> <td>${p[1]}</td>`);
     })
     console.log(`</table>`);
-
-    console.log
-    (
-`
-<br>
-<br>
-<i class='disclaimer'>Not an official source</i>
-<i class='disclaimer'>
-    <a href='https://github.com/dxwc/2020'>
-        Last Generated on ${new Date().toLocaleDateString()}
-    </a>
-</i>
-`
-    );
 })
 .then(() =>
 {
